2e3247ef8cdc0a6c111826780d535a145caca0bb
Mark Oosterveld <mark@oosterveld.org>
2021-06-23 14:51:30 -0400

    Update index.js

--- src/routes/[project]/featureFlag/[featureFlag]/index.js
+++ src/routes/[project]/featureFlag/[featureFlag]/index.js
@@ -1,10 +1,10 @@
 import taplytics from 'taplytics';
 
 const projects = {
-  '5fda25b259fc33000e629c03': 'c6ebc194de60e44c6148bbc3316b681ee2e88f32'
+  '5fda25b259fc33000e629c03': 'XXX'
 };
 
-const TAPLYTICS_SDK_KEY = 'c6ebc194de60e44c6148bbc3316b681ee2e88f32';
+const TAPLYTICS_SDK_KEY = 'XXX';
 
 export async function get({ params: { project, featureFlag } }) {
   const userid = 1;
